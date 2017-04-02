fe91bbb1a8df83cc60fc7dd385eef931cbc304b9
Maciej <maciej.jarmula@gmail.com>
2017-04-02 20:06:27 +0200

    Add web interface

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ import configparser
 # Called for every client connecting (after handshake)
 def new_client(client, server):
 	print("New client connected and was given id %d" % client['id'])
-	server.send_message_to_all("Hey all, a new client has joined us")
 
 
 # Called for every client disconnecting
---
+++ web/rpi_ws.js
@@ -0,0 +1,33 @@
+(function($){
+  var client_id ='xcfyt56fd';
+  var ws = new  WebSocket('ws://localhost:9001');
+  this.send = function (message, callback) {
+      this.waitForConnection(function () {
+          ws.send(JSON.stringify(message));
+          if (typeof callback !== 'undefined') {
+            callback();
+          }
+      }, 1000);
+  };
+
+  this.waitForConnection = function (callback, interval) {
+      if (ws.readyState === 1) {
+          callback();
+      } else {
+          var that = this;
+          setTimeout(function () {
+              that.waitForConnection(callback, interval);
+          }, interval);
+      }
+  };
+  this.send({
+    event: 'join_channel',
+    data: {
+      client_id: client_id
+    }
+  });
+  ws.onmessage = function(event){
+    data = JSON.parse(event.data)
+    $(document).trigger("rpi:message", [data.val])
+  }
+})(jQuery)
