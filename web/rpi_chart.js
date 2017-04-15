1be622ea8c2c12b61fb2a6fedd68afe6e0713dbd
Maciej Jarmula <maciej.jarmula@gmail.com>
2017-04-15 18:36:02 +0200

    Add charts

---
+++ web/rpi_chart.js
@@ -0,0 +1,49 @@
+function RpiChart(opts){
+  // type, dataPoints, color
+  /* point example:
+    { x: new Date(2010, 0, 3), y: 650 },
+  */
+  var selector = opts.type + "-chart"
+  var options = {
+    type: "line",
+    showInLegend: true,
+    lineThickness: 2,
+    name: opts.type,
+    color: opts.color || "#F08080",
+    dataPoints: opts.dataPoints || []
+  }
+  var chart = new CanvasJS.Chart(selector.toLowerCase(), {
+    title: {
+      text: opts.type,
+      fontSize: 30
+    },
+    animationEnabled: false,
+    axisX: {
+      gridColor: "Silver",
+      tickColor: "silver",
+      valueFormatString: "DD/MMM"
+    },
+    toolTip: {
+      shared: true
+    },
+    theme: "theme2",
+    axisY: {
+      gridColor: "Silver",
+      tickColor: "silver"
+    },
+    legend: {
+      verticalAlign: "bottom",
+      horizontalAlign: "center"
+    },
+    data: [options],
+  });
+  this.render = function(){
+    chart.render();
+    return this;
+  }
+  this.addPoint = function(value){
+    options.dataPoints.push({x: new Date(), y: value });
+    // $("#"+selector).CanvasJSChart(options);
+    return this;
+  }
+}
