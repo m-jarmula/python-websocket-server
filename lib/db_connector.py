1a8713e1165e1353ac910606a497359bc5ec6771
Maciej Jarmula <maciej.jarmula@gmail.com>
2017-06-09 23:20:51 +0200

    Add db adapter

--- lib/db_connector.py
+++ lib/db_connector.py
@@ -1,5 +1,17 @@
 import MySQLdb
-# db = MySQLdb.connect(host="localhost",
-#                      user="john",
-#                      passwd="megajonhy",
-#                      db="jonhydb")
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+class DBConnector:
+    def command(self, command):
+        conn = MySQLdb.connect(host=config['DB']["host"],
+                               user=config['DB']["user"],
+                               passwd=config['DB']["passwd"],
+                               db=config['DB']["db"])
+        cur = conn.cursor()
+        cur.execute(command)
+        conn.commit()
+        conn.close()
