da6c63aca7bd3d3b55106f209e7779f5102fd939
Maciej Jarmula <maciej.jarmula@gmail.com>
2017-06-09 23:21:19 +0200

    Add update database on report

--- lib/observer_manager.py
+++ lib/observer_manager.py
@@ -1,11 +1,12 @@
 from lib.message import Message
-
+from .db_connector import DBConnector
 
 class ObserverManager:
     def __init__(self, server):
         self.server = server
         server.em.add_event_listener('join_channel', self.on_join_channel)
         server.em.add_event_listener('report', self.on_report)
+        self.db = DBConnector()
 
     def on_join_channel(self, msg):
         if not msg['data']['client_id'] in self.server.channels:
@@ -15,8 +16,12 @@ class ObserverManager:
         client['client_id'] = msg['data']['client_id']
 
     def on_report(self, msg):
+        self.update_database(msg['data'])
         for user_id, user in self.server.channels[msg['client']['client_id']].items():
             # don't send message to yourself
             if(user_id != msg['client']['id']):
                 user['handler'].send_message(Message('report',
                                                      msg['data']).to_json())
+
+    def update_database(self, data):
+        self.db.command("INSERT INTO {} (id_sensor, value) VALUES({}, {})".format(data['type'], data['id_sensor'], data['value']))
